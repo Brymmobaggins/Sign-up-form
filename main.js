22518aa52908dd5a52322b502209b4b3f5dd58c6
Brymmobaggins <ibro2tablet21@yahoo.com>
2021-07-26 12:12:52 +0100

    fix password length and value

--- main.js
+++ main.js
@@ -15,33 +15,31 @@ function checkInputs() {
     const passwordValue = password.value.trim();
     const password2Value = password2.value.trim();
 
-    if (usernameValue === "") {
+    if (usernameValue === "") {                    // for username value
         setErrorFor(username, "username cannot be blank")
     } else {
         setSuccessFor(username)
     }
-    if (emailValue === "") {
+    if (emailValue === "") {                     // for email value
         setErrorFor(email, 'email cannot be blank')
-    // } else if (isValidEmail(emailValue)) {
-    //     setErrorFor(email, 'email is not valid')
     } else {
         setSuccessFor(email)
     }
-    if (passwordValue === "") {
+    if (passwordValue === "") {                 // for password value
         setErrorFor(password, 'password cannot be blank')
+    } else if
+        (passwordValue.length <= 7) {                 // for password length 
+        setErrorFor(password, 'password is too short')       
     } else {
         setSuccessFor(password)
     }
-    // if (passwordValue = 7) {
-    //     setErrorFor(password, 'password is too short')
-    // }
-    if (password2Value === "") {
+    if (password2Value === "") {                       // for password two value confirmation
         setErrorFor(password2, "password cannot be blank")
-    } else if (passwordValue !== password2Value) {
+    } else if (passwordValue !== password2Value) {        // for password comfirmation
         setErrorFor(password2, "password does not match")
     }
 }
-
+//  function for error messages
 function setErrorFor(input, message) {
     const formControl = input.parentElement; // .form-control
     const small = formControl.querySelector('small');
@@ -50,12 +48,12 @@ function setErrorFor(input, message) {
     //  add error class 
     formControl.className = 'form-control error';
 };
+//  function for success messages
 function setSuccessFor(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
 
-
-}
+};
 // function isValidEmail(email) {
 //     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 //     return re.test(String(email).toLowerCase())
